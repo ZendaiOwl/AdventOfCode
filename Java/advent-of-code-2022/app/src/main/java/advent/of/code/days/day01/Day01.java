c319d8dbb964ca257674b77aa6281803a608c225
Victor-ray, S <victorray91@pm.me>
2022-12-05 14:38:28 +0000

    Solved Day 01 puzzle
    Signed-off-by: Victor-ray, S <victorray91@pm.me>

--- Java/advent-of-code-2022/app/src/main/java/advent/of/code/days/day01/Day01.java
+++ Java/advent-of-code-2022/app/src/main/java/advent/of/code/days/day01/Day01.java
@@ -1,23 +1,43 @@
 package advent.of.code.days.day01;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Day01 {
-
-  private final static String TXTFile = "input.txt";
-
-  public static void readInputText() {
-    Scanner sc;
+  private final static String TXTFile = "./app/src/main/java/advent/of/code/days/day01/input.txt";
+  private static File inputTXT;
+  public static void getLargestAmountOfCalories() {
     try {
-      sc = new Scanner(TXTFile);
-    } catch (Exception e) {
-      System.out.println(e);
-      return;
+      inputTXT = new File(TXTFile);
+    } catch(Exception e) {
+      System.err.println(e);
     }
-    sc.close();
+    int largestAmountOfCalories = 0;
+    try {
+      Scanner sc = new Scanner(inputTXT);
+      int count = 0, elf = 0;
+      while (sc.hasNextLine()) {
+        String line = sc.nextLine();
+        int calories = 0;
+        count++;
+        while(!line.equals("") && sc.hasNextLine()) {
+          calories += Integer.parseInt(line);
+          line = sc.nextLine();
+        }
+        if(calories > largestAmountOfCalories) {
+          largestAmountOfCalories = calories;
+          elf = count;
+        }
+      }
+      System.out.println("Largest amount of calories: " + largestAmountOfCalories + "\n" +
+          "Carried by Elf: " + elf + "\n");
+      sc.close();
+    } catch (Exception e) {
+      System.err.println(e);
+    }
   }
 
   public static void main(String[] args) {
-    
+    getLargestAmountOfCalories();
   }
 }
