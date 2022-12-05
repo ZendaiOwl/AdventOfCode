a185c49ab0015d61e644cd5cfaf48384cfeae79d
Victor-ray, S <victorray91@pm.me>
2022-12-05 15:06:54 +0000

    Solved Day 01 Part 2
    Signed-off-by: Victor-ray, S <victorray91@pm.me>

--- Java/advent-of-code-2022/app/src/main/java/advent/of/code/days/day01/Day01.java
+++ Java/advent-of-code-2022/app/src/main/java/advent/of/code/days/day01/Day01.java
@@ -1,6 +1,8 @@
 package advent.of.code.days.day01;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Day01 {
@@ -37,7 +39,36 @@ public class Day01 {
     }
   }
 
+  public static void getTopThreeElvesCaloriesCount() {
+    ArrayList<Integer> allCalories = new ArrayList<>();
+    try {
+      inputTXT = new File(TXTFile);
+    } catch(Exception e) {
+      System.err.println(e);
+    }
+    try {
+      Scanner sc = new Scanner(inputTXT);
+        while(sc.hasNextLine()) {
+          int calories = 0;
+          String line = sc.nextLine();
+            while(!line.equals("") && sc.hasNextLine()) {
+              calories += Integer.parseInt(line);
+              line = sc.nextLine();
+            }
+            allCalories.add(calories);
+        }
+      sc.close();
+      Collections.sort(allCalories);
+      int size = (allCalories.size() - 1);
+      int calculatedCaloriesTop3 = (allCalories.get(size) + allCalories.get(size-1) + allCalories.get(size-2));
+      System.out.println("Calories count for the top 3 Elves: " + calculatedCaloriesTop3 + "\n");
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+  }
+
   public static void main(String[] args) {
     getLargestAmountOfCalories();
+    getTopThreeElvesCaloriesCount();
   }
 }
