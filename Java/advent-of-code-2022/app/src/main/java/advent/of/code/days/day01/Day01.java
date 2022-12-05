bea8f887a054a14e3ae319bf2efe0b18249507a0
Victor-ray, S <victorray91@pm.me>
2022-12-05 13:50:54 +0000

    Started working on day 1
    Signed-off-by: Victor-ray, S <victorray91@pm.me>

--- Java/advent-of-code-2022/app/src/main/java/advent/of/code/days/day01/Day01.java
+++ Java/advent-of-code-2022/app/src/main/java/advent/of/code/days/day01/Day01.java
@@ -1,5 +1,23 @@
 package advent.of.code.days.day01;
 
+import java.util.Scanner;
+
 public class Day01 {
-  
+
+  private final static String TXTFile = "input.txt";
+
+  public static void readInputText() {
+    Scanner sc;
+    try {
+      sc = new Scanner(TXTFile);
+    } catch (Exception e) {
+      System.out.println(e);
+      return;
+    }
+    sc.close();
+  }
+
+  public static void main(String[] args) {
+    
+  }
 }
